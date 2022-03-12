4ffdbf4a658e6cf2afef44c022c4efff3cd611c2
Anton Kotlyakov <an.kotlyakov@yandex.ru>
2022-03-12 12:27:30 +0300

    feat: Сделан функционал по 4 работе

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+let btnPopupEdit = document.querySelector(".profile__btn-edit");
+let btnPopupClose = document.querySelector(".popup__btn-close");
+let popup = document.querySelector(".popup");
+let popupInput = document.querySelectorAll(".popup__input");
+let [inputName, inputAbout] = popupInput;
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+let popupForm = document.querySelector(".popup__container");
+
+function popupOpen() {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+  popup.classList.add("popup_opened");
+}
+
+function popupClose() {
+  popup.classList.remove("popup_opened");
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  popupClose();
+}
+
+function popupCloseBackground(evt) {
+  evt.stopPropagation();
+}
+
+btnPopupEdit.addEventListener("click", popupOpen);
+btnPopupClose.addEventListener("click", popupClose);
+popupForm.addEventListener("click", popupCloseBackground);
+popupForm.addEventListener("submit", formSubmitHandler);
+popup.addEventListener("click", popupClose);
