7c26d256b42b6b261f7f5a92412f241f7b387f74
Anton Kotlyakov <an.kotlyakov@yandex.ru>
2022-03-21 15:47:22 +0300

    feat: Добавлен popup добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 let btnPopupEdit = document.querySelector(".profile__btn-edit");
-let btnPopupClose = document.querySelector(".popup__btn-close");
-let popup = document.querySelector(".popup");
+let btnPopupClose = document.querySelectorAll(".popup__btn-close");
+let btnPopupAdd = document.querySelector(".profile__btn-add");
+let popup = document.querySelectorAll(".popup");
+let popupEdit = document.querySelector(".popup_edit");
+let popupAdd = document.querySelector(".popup_add");
 let popupInput = document.querySelectorAll(".popup__input");
 let inputName = document.querySelector("[name=inputName]");
 let inputAbout = document.querySelector("[name=inputAbout]");
@@ -8,14 +11,45 @@ let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about");
 let popupForm = document.querySelector("[name=editFormProfile]");
 
-function popupOpen() {
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+function popupEditOpen() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
-  popup.classList.add("popup_opened");
+  popupEdit.classList.add("popup_opened");
+}
+
+function popupAddOpen() {
+  popupAdd.classList.add("popup_opened");
 }
 
 function popupClose() {
-  popup.classList.remove("popup_opened");
+  popup.forEach((item) => item.classList.remove("popup_opened"));
 }
 
 function formSubmitHandler(evt) {
@@ -25,6 +59,7 @@ function formSubmitHandler(evt) {
   popupClose();
 }
 
-btnPopupEdit.addEventListener("click", popupOpen);
-btnPopupClose.addEventListener("click", popupClose);
+btnPopupAdd.addEventListener("click", popupAddOpen);
+btnPopupEdit.addEventListener("click", popupEditOpen);
+btnPopupClose.forEach((item) => item.addEventListener("click", popupClose));
 popupForm.addEventListener("submit", formSubmitHandler);
